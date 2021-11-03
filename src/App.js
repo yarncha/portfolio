07bf38f5aaf53d78e83d9ef4e776809ee197f742
YAMCHA <dbals9812@naver.com>
2021-11-04 02:26:52 +0900

    edit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,26 @@
-import logo from './logo.svg';
+import React, { useState } from "react";
+import { Switch, Route, Link } from "react-router-dom";
+
+// CSS
 import './App.css';
+
+// Components
+import { MainPage } from "./components/MainPage.js";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="nav-var">
+        
+      </div>
+
+      <Switch>
+        <Route path="/">
+          <MainPage />
+        </Route>
+      </Switch>
+
+<footer></footer>
     </div>
   );
 }
---
+++ src/components/MainPage.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+// import "./MainPage.css";
+
+function MainPage() {
+
+    return (
+        <div>
+            <p>안녕하세요!</p>
+            <p>가장 간결하게 한 페이지에 볼 수 있도록 넣기</p>
+        </div>
+    );
+}
+
+export { MainPage };
